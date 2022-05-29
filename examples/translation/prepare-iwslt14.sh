6fa46a195a9aeb796d2c7c6524004945083bf99a
Nguyễn Quốc Bảo <91514031+BaoNguyenHCMUS@users.noreply.github.com>
2022-05-29 15:36:57 +0700

    edit scripts

--- examples/translation/prepare-iwslt14.sh
+++ examples/translation/prepare-iwslt14.sh
@@ -15,7 +15,7 @@ CLEAN=$SCRIPTS/training/clean-corpus-n.perl
 BPEROOT=subword-nmt/subword_nmt
 BPE_TOKENS=10000
 
-URL="http://dl.fbaipublicfiles.com/fairseq/data/iwslt14/de-en.tgz"
+URL="http://dl.fbaipublicfiles.com/fairseq/data/iwslt14/de-en.tgz"\n
 GZ=de-en.tgz
 
 if [ ! -d "$SCRIPTS" ]; then
